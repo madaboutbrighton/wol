d6e6d4dda587850f65b75d86a3a569619e2f2dd8
mab <madaboutbrighton@users.noreply.github.com>
2016-06-20 18:04:56 +0100

    Update wol.sh

--- wol.sh
+++ wol.sh
@@ -13,18 +13,18 @@ DO_WOL=1
 DO_MYSQL=1
 
 # Backend settings
-SERVER_MAC='90:2B:34:AE:B5:A0'
-SERVER_IP='192.168.1.10'
+SERVER_MAC='MA:CA:DD:RE:SS:00'
+SERVER_IP='192.168.999.666'
 
 # mySQL settings on backend
 MYSQL_USER='dummy'
-MYSQL_PASS='12dummy34'
+MYSQL_PASS='mydummymysqlpassword'
 
 # Wait settings (in seconds)
 WAIT_LAN=1
 WAIT_MYSQL=5
 WAIT_FINAL=5
-WAIT_MAX=300
+WAIT_MAX=40
 
 ## Not recommended to change anything below this line ##
 
@@ -35,7 +35,7 @@ if [ $DO_WOL -eq 1 ]; then
   # Issue wakeonlan at intervals until our own network interface
   #  is active and the magic packet is successfully sent.
   #
-  echo "Waiting for connection with $SERVER_IP($SERVER_MAC)"
+  echo "Waiting for client network interface to become active"
 
   until /usr/bin/wakeonlan $SERVER_MAC > /dev/null 2>&1 ; do
   
@@ -49,7 +49,30 @@ if [ $DO_WOL -eq 1 ]; then
     sleep $WAIT_LAN
   done
 
-  echo "Connected to $SERVER_IP"
+  echo "  -Client network interface is active"
+  
+  #
+  # Issue ping at intervals until reply received
+  #
+  echo "Waiting for reply from $SERVER_IP (MAC - $SERVER_MAC)"
+  
+  pingcount=0
+  
+  until [ ${pingcount:-0} -gt 0 ]; do
+  
+    if [ $timesofar -gt $WAIT_MAX ]; then
+      echo "Waited too long"
+      exit 0;
+    fi
+  
+    pingcount=$(ping -c1 $SERVER_IP | grep 'received' | awk -F ',' '{print $2}' | awk '{ print $1}')
+  
+    timesofar=$((timesofar + WAIT_LAN))
+
+    sleep $WAIT_LAN
+  done
+
+  echo "  -Reply received"
 fi
 
 if [ $DO_MYSQL -eq 1 ]; then
@@ -70,7 +93,7 @@ if [ $DO_MYSQL -eq 1 ]; then
     sleep $WAIT_MYSQL
   done
 
-  echo "Connected to mySQL on $SERVER_IP"
+  echo "  -Connected to mySQL on $SERVER_IP"
   
   # An extra wait is required if mySQL was not already running
   if [ $timesofar -gt 0 ]; then
